ec8e4edfc2cc6238fc2bc5ade0c49b475f1704a5
Oscar Raichert <oscarraichert@hotmail.com>
2023-12-02 01:55:16 -0300

    day 1 challenge 2

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
 
     #[test]
     fn trebuchet_01() {
@@ -23,5 +24,98 @@ mod tests {
         }
 
         println!("{result}");
+    }
+
+    #[test]
+    fn trebuchet_02() {
+        let result = calculate_calibration(include_str!("trebuchet_input.txt"));
+
+        assert_eq!(result, 54980);
+    }
+
+    #[test]
+    fn trebuchet_02_ex() {
+        let result = calculate_calibration(include_str!("trebuchet_input_ex_02.txt"));
+
+        assert_eq!(result, 281);
+    }
+
+    #[test]
+    fn trebuchet_ex_line_02() {
+        let result = calculate_calibration("eightwothree");
+
+        assert_eq!(result, 83);
+    }
+
+    fn calculate_calibration(input: &str) -> u32 {
+        let input_lines = input.lines();
+
+        let nums: HashMap<_, u32> = HashMap::from([
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ]);
+
+        let mut result = 0;
+
+        for line in input_lines {
+            result += get_line_calibration(line, &nums);
+        }
+        result
+    }
+
+    fn get_line_calibration(line: &str, nums: &HashMap<&str, u32>) -> u32 {
+        let mut line_coordinates: Vec<(usize, u32)> = Vec::new();
+
+        let digits_char: Vec<_> = line.chars().filter(|x| x.is_digit(10)).collect();
+
+        if digits_char.len() > 0 {
+            let first_digit_char = digits_char.first().expect("no number").to_owned();
+            let last_digit_char = digits_char.last().expect("no number").to_owned();
+
+            let mut first_digit: Vec<_> = line
+                .match_indices(first_digit_char)
+                .flat_map(|x| x.1.parse::<u32>().map(|y| (x.0, y)))
+                .collect();
+
+            let mut last_digit: Vec<_> = line
+                .match_indices(last_digit_char)
+                .flat_map(|x| x.1.parse::<u32>().map(|y| (x.0, y)))
+                .collect();
+
+            line_coordinates.append(&mut first_digit);
+            line_coordinates.append(&mut last_digit);
+        }
+
+        for extensive_num in nums.keys() {
+            if line.contains(extensive_num) {
+                let mut extensive_num_index: Vec<_> = line
+                    .match_indices(extensive_num)
+                    .map(|x| (x.0, nums.get(x.1).expect("no conversion").to_owned()))
+                    .collect();
+
+                line_coordinates.append(&mut extensive_num_index);
+            }
+        }
+
+        let first_num = line_coordinates
+            .iter()
+            .min_by(|a, b| a.0.cmp(&b.0))
+            .expect("nonexistent")
+            .1;
+
+        let last_num = line_coordinates
+            .iter()
+            .max_by(|a, b| a.0.cmp(&b.0))
+            .expect("nonexistent")
+            .1;
+
+        first_num * 10 + last_num
     }
 }
