d50d0d66d7456c40892419cdfa99c0781400d622
Oscar Raichert <oscarraichert@hotmail.com>
2023-12-07 02:02:29 -0300

    pegando os simbolos

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -12,20 +12,28 @@ mod tests {
         let y_size = lines.clone().collect::<Vec<_>>().len();
 
         let mut digits: Vec<_> = Vec::new();
+        let mut symbols: Vec<_> = Vec::new();
 
         for line in lines.enumerate() {
             let chars = line.1.chars();
             let char_enum = chars.enumerate();
 
             let mut line_digits: Vec<_> = char_enum
+                .to_owned()
                 .filter_map(|c| c.1.to_digit(10).map(|v| (line.0, c.0, v)))
                 .collect();
 
+            let mut line_symbols: Vec<_> = char_enum
+                .filter(|c| !c.1.is_digit(10))
+                .filter(|c| c.1 != '.')
+                .map(|s| (line.0, s.0, s.1))
+                .collect();
+
             digits.append(&mut line_digits);
+            symbols.append(&mut line_symbols);
         }
 
-        digits.get(0)
-
-        println!("{digits:?}")
+        println!("{digits:?}");
+        println!("{symbols:?}")
     }
 }
